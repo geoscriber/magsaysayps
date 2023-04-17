8b40e30603e029a657a9f2ac579aa3dcb510a117
krquinton <nscrexgst.dotr@gmail.com>
2023-04-17 14:13:15 +0800

    initial commit

---
+++ data.js
@@ -0,0 +1,137 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-magsaysay-1",
+      "name": "Magsaysay 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.08100865616603592,
+        "pitch": 1.0056212945829905,
+        "fov": 1.4082576482174474
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.22027947556244243,
+          "pitch": 0.7797084553569924,
+          "rotation": 0,
+          "target": "2-magsaysay-3"
+        },
+        {
+          "yaw": 0.39241944977279886,
+          "pitch": 0.41566164298209074,
+          "rotation": 0,
+          "target": "1-magsaysay-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-magsaysay-2",
+      "name": "Magsaysay 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.01745329251995109,
+        "pitch": 0.8854411338517352,
+        "fov": 1.4082576482174474
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2657087689227655,
+          "pitch": 0.9759640701077643,
+          "rotation": 0,
+          "target": "2-magsaysay-3"
+        },
+        {
+          "yaw": -0.07290604789140076,
+          "pitch": 0.030182721606378138,
+          "rotation": 0,
+          "target": "0-magsaysay-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.09842506097022685,
+          "pitch": 0.9465497499282876,
+          "title": "Title",
+          "text": "Pumping Station"
+        }
+      ]
+    },
+    {
+      "id": "2-magsaysay-3",
+      "name": "Magsaysay 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.14031464743052346,
+        "pitch": 0.35890668428002925,
+        "fov": 1.4082576482174474
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Magsaysay Pumping Station",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
